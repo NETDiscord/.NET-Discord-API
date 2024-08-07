8c6863458229c89ebe80bb0ba4de8642d8c44137
Rex Aliis <rexaliisdev@gmail.com>
2024-08-06 23:55:01 -0300

    feat: add an API definition base class
    Add a class that all API definitions must inherit.
    
    To provide the necessary implementations across all definitions.

---
+++ NETDiscord.Api/APIObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using TupleJsonConverters;
+
+namespace NETDiscord.Api
+{
+	public abstract class ApiObject
+	{
+		private static readonly JsonSerializerOptions _options = new()
+		{
+			IncludeFields = false,
+			WriteIndented = false,
+			AllowTrailingCommas = false,
+			NumberHandling = JsonNumberHandling.Strict,
+			ReadCommentHandling = JsonCommentHandling.Skip,
+			UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip,
+			DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+			DictionaryKeyPolicy = JsonNamingPolicy.SnakeCaseLower,
+			PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+			Converters = { new TupleConverter() },
+			TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+			{
+				Modifiers = { SerializeLongAsString }
+			}
+		};
+
+		private static void SerializeLongAsString(JsonTypeInfo jsonTypeInfo)
+		{
+			if (jsonTypeInfo.Type == typeof(long) || jsonTypeInfo.Type == typeof(ulong))
+			{
+				jsonTypeInfo.NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString;
+			};
+		}
+		public override string ToString() => JsonSerializer.Serialize(this, GetType(), _options);
+		public static T? FromString<T>(string json) where T : ApiObject => JsonSerializer.Deserialize<T>(json, _options);
+	}
+}
